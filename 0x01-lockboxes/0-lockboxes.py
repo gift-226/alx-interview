c75b35339d6dab744f6bb1a4541326904832b8b0
gift-226 <giftonwuanaku@gmail.com>
2023-01-31 09:32:09 +0100

    y

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unlocks list of lists"""
+
+
+def canUnlockAll(boxes):
+    """Takes a list of lists and the content of the list will unlock other lists"""
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
